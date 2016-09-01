a777d36c1250b2c3d1a9f37c7d7b95e8b1c60164
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2016-09-01 17:52:38 +0000

    checkbox to switch

--- src/com/android/gallery3d/app/GallerySettings.java
+++ src/com/android/gallery3d/app/GallerySettings.java
@@ -25,7 +25,7 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import android.preference.CheckBoxPreference;
+import android.preference.SwitchPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
